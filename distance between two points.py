2cbac20b4d83dd3e6111b841738a0219332961e1
KhoobBabe <77692425+KhoobBabe@users.noreply.github.com>
2021-07-24 23:16:43 +0500

    Add files via upload

---
+++ distance between two points.py
@@ -0,0 +1,14 @@
+def points_distance(x, y):
+    print(“The distance between points is:” (x + y)**0.5)
+    return
+
+
+
+x_1 = float(input("enter x1:"))
+y_1 = float(input("enter y1:"))
+
+x_2 = float(input("enter x2:"))
+y_2 = float(input("enter y2:"))
+                
+x= (x_2-x_1)**2
+y= (y_2-y_1)**2
